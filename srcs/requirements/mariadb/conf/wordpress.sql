fd58f4ce7b3b6e08eb4e95a0add36eb0c95bfafd
Maxim <lolesqua@gmail.com>
2023-04-07 05:43:56 -0500

    ✨ MariaDB docker container

---
+++ srcs/requirements/mariadb/conf/wordpress.sql
@@ -0,0 +1,36 @@
+-- Switch to the 'mysql' database
+USE mysql;
+
+-- Reload MySQL privilege tables to apply changes
+FLUSH PRIVILEGES;
+
+-- Delete any MySQL user accounts with empty usernames
+DELETE FROM mysql.user WHERE User='';
+
+-- Drop the 'test' database
+DROP DATABASE test;
+
+-- Delete any references to the 'test' database in the mysql.db table
+DELETE FROM mysql.db WHERE Db='test';
+
+-- Delete MySQL user accounts with 'root' username and host not in ('localhost', '127.0.0.1', '::1')
+-- This is done to restrict access of root user to local connections only
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+-- Update the password for the MySQL root user for connections from localhost
+-- The new password is specified in the ${DB_ROOT_PASS} variable
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+
+-- Create a new MySQL database with the name specified in the ${DB_NAME} variable
+-- The character set and collation are set to utf8 and utf8_general_ci, respectively
+CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- Create a new MySQL user with the username and password specified in the ${DB_USER} and ${DB_PASS} variables
+-- The user is granted access from any host (%)
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+
+-- Grant all privileges on the 'wordpress' database to the MySQL user created in the previous statement
+GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+
+-- Reload MySQL privilege tables to apply changes
+FLUSH PRIVILEGES;
---
+++ srcs/requirements/mariadb/scripts/database.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+
+	echo "[INFO] Initializing MariaDB database..."
+
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+fi
+
+# Setup Wordpress database if it doesn't exist
+if [ ! -d /var/lib/mysql/wordpress ]; then
+
+	echo "[INFO] Creating Wordpress database..."
+
+	# Replace environment variables in SQL file
+	cp /tmp/wordpress.sql /tmp/wordpress.sql.tmp
+	envsubst < /tmp/wordpress.sql.tmp > /tmp/wordpress.sql
+
+	# --bootstrap is used to execute the SQL commands without starting the server
+	/usr/bin/mysqld --user=mysql --bootstrap < /tmp/wordpress.sql
+
+	# Remove temporary file
+	rm -rf /tmp/wordpress.sql
+
+fi
+
+echo "[INFO] Starting MariaDB..."
+
+# Start MariaDB
+/usr/bin/mysqld --skip-log-error
